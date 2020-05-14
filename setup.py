9de0f589424225f78af5f4f40d304fea64c16c89
ahcorde <ahcorde@gmail.com>
2020-05-14 12:33:09 +0200

    Fixed package to run with ros2 run
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- setup.py
+++ setup.py
@@ -1,12 +1,39 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+package_name = 'rqt_plot'
 
-d = generate_distutils_setup(
-    packages=['rqt_plot', 'rqt_plot.data_plot'],
+setup(
+    name=package_name,
+    version='1.0.7',
+    packages=[package_name, package_name + '/data_plot'],
     package_dir={'': 'src'},
-    scripts=['scripts/rqt_plot']
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name + '/resource',
+            ['resource/plot.ui']),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['plugin.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Dorian Scholz',
+    maintainer='Dirk Thomas, Dorian Scholz, Austin Hendrix',
+    maintainer_email='dthomas@osrfoundation.org',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description=(
+        'RQT plugin for monitoring ROS processes.'
+    ),
+    license='BSD',
+    entry_points={
+        'console_scripts': [
+            'rqt_plot = ' + package_name + '.main:main',
+        ],
+    },
 )
-
-setup(**d)
