15b189d3d6f5ebd4410f534e25473871043457af
Shane Loretz <sloretz@openrobotics.org>
2020-05-20 13:14:13 -0400

    Use catkin_install_python() (#59)
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['rqt_plot', 'rqt_plot.data_plot'],
-    package_dir={'': 'src'},
-    scripts=['scripts/rqt_plot']
+    package_dir={'': 'src'}
 )
 
 setup(**d)
