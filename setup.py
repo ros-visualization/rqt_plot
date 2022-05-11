db90a5ff90bdb13e10eedcd1e4b5cbd7642fb66e
Audrow Nash <audrow@hey.com>
2022-05-10 17:36:22 -0700

    1.2.0
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'rqt_plot'
 
 setup(
     name=package_name,
-    version='1.1.1',
+    version='1.2.0',
     packages=[package_name, package_name + '/data_plot'],
     package_dir={'': 'src'},
     data_files=[
