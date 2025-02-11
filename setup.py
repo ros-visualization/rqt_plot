41ce64caa11837ad2732c95024c2601eeb16d3a1
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-02-11 13:26:13 +0100

    1.4.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'rqt_plot'
 
 setup(
     name=package_name,
-    version='1.4.0',
+    version='1.4.1',
     packages=[package_name, package_name + '/data_plot'],
     package_dir={'': 'src'},
     data_files=[
