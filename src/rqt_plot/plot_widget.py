6f20599da0f43e70299eacc486819dd0d9d6d6a1
Christophe Bedard <bedard.christophe@gmail.com>
2025-02-11 06:32:52 -0800

    Fix f-string and add single quote around field name (#100)
    This is more consistent with the other messages.
    
    Signed-off-by: Christophe Bedard <christophe.bedard@apex.ai>
    (cherry picked from commit 6a60d2a16bc307c81c309f819070aee52d67d745)
    
    # Conflicts:
    #	src/rqt_plot/plot_widget.py

--- src/rqt_plot/plot_widget.py
+++ src/rqt_plot/plot_widget.py
@@ -125,7 +125,11 @@ def get_plot_fields(node, topic_name):
 
         if is_array_or_sequence:
             if not has_index:
+<<<<<<< HEAD
                 return [], base_error_msg + f'{name} is a nested type but no index provided'
+=======
+                return [], base_err_msg + f"'{name}' is a nested type but no index provided"
+>>>>>>> 6a60d2a (Fix f-string and add single quote around field name (#100))
 
             if current_type.has_maximum_size():
                 # has_maximum_size() doesn't necessarily mean that the object has a 'maximum_size' field. The meaning
@@ -137,7 +141,11 @@ def get_plot_fields(node, topic_name):
                         f"index '{index}' out of bounds, maximum size is {size}")
             current_type = current_type.value_type
         elif has_index:
+<<<<<<< HEAD
             return [], base_error_msg + "{name} is not an array or sequence"
+=======
+            return [], base_err_msg + f"'{name}' is not an array or sequence"
+>>>>>>> 6a60d2a (Fix f-string and add single quote around field name (#100))
 
         if not isinstance(current_type, NamespacedType):
             break
